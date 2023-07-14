fa4f66c009be6650349c993b2dccc2edb4888e80
Iago Mac <iaemac.ig@gmail.com>
2023-07-14 15:20:50 -0300

    Update script.js
    mudando arquivo javaScript para que apareça mensagem de erro ou sucesso sem estarem juntas

--- script.js
+++ script.js
@@ -8,12 +8,13 @@ form.addEventListener('submit' , function(e){
 
     if (numeroB>numeroA){
         document.querySelector(".sucess-menssage").style.display = "block";
+        document.querySelector(".error-message").style.display = "none";
 
         document.getElementById('numero-a').value = "";
         document.getElementById('numero-b').value = "";
 
     }else{
         document.querySelector(".error-menssage").style.display = "block";
+        document.querySelector(".sucess-menssage").style.display = "none";
     }
 });
-
